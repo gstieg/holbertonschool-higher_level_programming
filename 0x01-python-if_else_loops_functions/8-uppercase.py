c92d525bb7cca1a38bc9488b6e3f52a7c69042f5
Grayson Stiegler <682@holbertonschool.com>
2019-05-01 21:38:33 +0000

    added syntax

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
+        c = ord(i)
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
             c = ord(i) - 32
-        else:
-            c = ord(i)
         print(chr(c), end="")
     print('')
