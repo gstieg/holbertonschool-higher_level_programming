f1cec9458f197eedd77dec5b38cc842fa1b79cbd
gstieg <45178082+gstieg@users.noreply.github.com>
2019-05-01 16:18:26 -0700

    Delete 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        c = ord(i)
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            c = ord(i) - 32
-        print(chr(c), end="")
-    print('')
