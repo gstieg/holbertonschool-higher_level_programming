ed09ac0aa31f957f747bd705e454cf9891d66fdb
Grayson Stiegler <682@holbertonschool.com>
2019-05-07 21:36:12 +0000

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = []
-    if idx < 0 or idx >= len(new_list):
+    if idx >= 0 or idx < len(new_list):
         for num in my_list:
             new_list.append(num)
         new_list[idx] = element
