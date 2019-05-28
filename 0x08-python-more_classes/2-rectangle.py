e0559407e5f572db6f1ae32f710db1776c972b45
Grayson Stiegler <682@holbertonschool.com>
2019-05-28 22:41:29 +0000

    fix typos

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,38 +9,38 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-        @property
-        def width(self):
-            """property of width"""
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """sets the width"""
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-                self.__width = value
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-                """gets height of rectangle"""
-                return self.__height
+    @property
+    def height(self):
+        """gets height of rectangle"""
+        return self.__height
 
-        @height.setter
-        def height(self,value):
-                """sets height"""
+    @height.setter
+    def height(self,value):
+        """sets height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-                if type(value) is not int:
-                    raise TypeError("height must be an integer")
-                elif value < 0:
-                    raise ValueError("height must be >= 0")
-                    self.___height = value
+    def area(self):
+        """finds the area"""
+        return self.__width * self.__height
 
-                    def area(self):
-                        """finds the area"""
-                        return self.__width * self.__height
-                def perimeter(self):
-                    """ gets the perimeter"""
-                    return 2 * self.__width + 2 * self.__height
+    def perimeter(self):
+        """ gets the perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return self.__height * 2 + self.__width * 2
