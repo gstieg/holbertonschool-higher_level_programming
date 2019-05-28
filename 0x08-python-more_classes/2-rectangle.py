a6050843448e1e12345aff634b65810c1eef972a
Grayson Stiegler <682@holbertonschool.com>
2019-05-28 21:45:08 +0000

    Write a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """args: widht and height
+        width = width of rectanglw
+        height = height of Rectangl"""
+        self.__width = width
+        self.__height = height
+
+        @property
+        def width(self):
+            """property of width"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """sets the width"""
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+                self.__width = value
+
+        @property
+        def height(self):
+                """gets height of rectangle"""
+                return self.__height
+
+        @height.setter
+        def height(self,value):
+                """sets height"""
+
+                if type(value) is not int:
+                    raise TypeError("height must be an integer")
+                elif value < 0:
+                    raise ValueError("height must be >= 0")
+                    self.___height = value
+
+                    def area(self):
+                        """finds the area"""
+                        return self.__width * self.__height
+                def perimeter(self):
+                    """ gets the perimeter"""
+                    return 2 * self.__width + 2 * self.__height
