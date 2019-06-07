af398871dd0511b2d769d32026161d735d2e3fbe
Grayson Stiegler <682@holbertonschool.com>
2019-06-07 02:21:27 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout:"""
+"""Write a function that reads a text file (UTF8) and prints it to stdout:"""
 
-    """ args:
-    filename: name of the file
-    """
-    with open(filename, encoding='utf-8') as file1:
-        print(file1.read())
+
+def read_file(filename=""):
+    """ agrs:
+    filename = name of the file"""
+
+    with open(filename, encoding='utf-8') as a:
+        for line in a:
+            print(line, end="")
