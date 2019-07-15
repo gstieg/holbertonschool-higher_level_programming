7384ec4d6e0e5c0aac846e98a4cf6976f95b3fac
Grayson Stiegler <682@holbertonschool.com>
2019-07-15 22:24:40 +0000

    fix requirement checks

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,10 +1,7 @@
 -- lists all records in the table second_table
 SELECT score, name
-
 FROM
 	second_table
-
-WHERE score >= 10
-
-ORDER BY
-      DESC
+WHERE
+      score >= 10
+ORDER BY DESC
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 --  lists the number of records with the same score in the table second_table'
-SELECT score, COUNT(*) AS NUMBER
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY NUMBER DESC
+ORDER BY number DESC
