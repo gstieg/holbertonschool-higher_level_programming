e837f05124a816e06e6030885a7a8b519b7700bd
Grayson Stiegler <682@holbertonschool.com>
2019-07-16 20:06:58 +0000

    fix spelling error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates new user and gives them privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIES BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
