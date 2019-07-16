8c59acd6524cf132b383d48e5ec36a6007e33792
Grayson Stiegler <682@holbertonschool.com>
2019-07-16 22:48:35 +0000

    uses the hbtn_0d_tvshows database to lists all genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- uses the hbtn_0d_tvshows database to lists all genres
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name ASC;
