b84c1c9b9cfefe9d3dd9f6c8fbd886baa885165f
Grayson Stiegler <682@holbertonschool.com>
2019-08-11 18:15:13 +0000

    fix eol of string literal

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     new = conn.cursor()
-    new.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    new.execute("SELECT\
+    * FROM states WHERE name LIKE 'N%'\
+    ORDER BY states.id ASC")
     for row in new.fetchall():
         print(row)
     new.close()
