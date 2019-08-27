6c0e462af4ed763db748850be7787196c1cb8ca3
Grayson Stiegler <682@holbertonschool.com>
2019-08-27 04:08:55 +0000

    re run authentication

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""takes your Github credentials (username and password) and uses the Github API
-to display your id"""
+"""takes your Github credentials (username and password) and uses the Github
+API to display your id"""
 import requests
 from sys import argv
 
 if __name__ == "__main__":
     try:
-        new = requests.get('https://api.github.com/user',
-                       auth=(argv[1], argv[2]))
+        new = requests.get("https://api.github.com/user",
+                           auth=(argv[1], argv[2])).json()
         print(new.get('id'))
     except:
         print("None")
